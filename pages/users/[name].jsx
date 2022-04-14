998f7fdfcf1db1dc7106c367fa2fd1ba66be60ab
donghyeonGo <donghyeon.go@fastlane.kr>
2022-04-14 15:36:48 +0900

    git정보 로드 react icons  활용 화면에 프로필 스타일링

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,28 +1,92 @@
+import css from 'styled-jsx/css';
 import fetch from 'isomorphic-unfetch';
+import { GoMail } from 'react-icons/go';
+
+const style = css`
+    h2 {
+        margin-left: 20px;
+    }
+    .user-bio{
+        margin-top: 12px;
+        font-style: italic;
+    }
+    .profile-box{
+        width: 25%;
+        max-width: 272px;
+        margin-right: 26px;
+    }
+    .profile-image-wrapper{
+        width: 100%;
+        border: 1px solid #e1e4e8;
+    }
+    .profile-image-wrapper .profile-image{
+        display: block;
+        width: 100%;
+    }
+    .profile-username{
+        margin: 0;
+        padding-top: 16px;
+        font-size: 26px;
+    }
+    .profile-user-login{
+        margin: 0;
+        font-size: 20px;
+    }
+    .profile-user-bio{
+        margin: 0;
+        padding-top: 16px;
+        font-size: 14px;
+    }
+    .profile-user-info{
+        display: flex;
+        align-items: center;
+        margin: 4px 0 0;
+    }
+    .profile-user-info-text{
+        margin-left: 6px;
+    }
+`;
 
 const name = ({ user }) => {
-    console.log(user.name);
-    const username = user && user.name;
-    console.log(username);
-    return <div>{username}</div>
+    if (!user) {
+        return null;
+    }
+    return (
+        <>
+            <div className='profile-box'>
+                <div className='profile-image-wrapper'>
+                    <img
+                        className='profile-image'
+                        src={user.avatar_url}
+                        alt={`${user.name} 프로필 이미지`}
+                    />
+                </div>
+                <h2 className='profile-username'>{user.name}</h2>
+                <p className='profile-user-login'>{user.login}</p>
+                <p className='profile-user-bio'>{user.bio}</p>
+                <p className='profile-user-info'>
+                    <GoMail size={16} color="#6a737d" />
+                    <span className='profile-user-info-text'>{user.email}</span>
+                </p>
+            </div>
+            <style jsx>{style}</style>
+        </>
+    );
 };
 
-name.getInitialProps = async ({ query }) => {
+export const getServerSideProps = async ({ query }) => {
     const { name } = query;
     try {
         const res = await fetch(`https://api.github.com/users/${name}`);
         if (res.status === 200) {
-            console.log("ddd");
-
             const user = await res.json();
-            console.log(`user ${user.name}`)
-            return { user };
+            return { props: { user } }
         }
-        return { props: {} };
+        return { props: { user } };
     } catch (e) {
         console.log(e);
-        return {};
-    }
+        return { props: {} };
+    };
+};
 
-}
 export default name;
